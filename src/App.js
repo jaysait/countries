585f1c3491113fed209891306b6dfc837c15ed51
Jason Fleming <jason.fleming.se@gmail.com>
2021-10-30 14:39:02 -0600

    exercise2.12 rest api for countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Country from './components/Country';
 
-function App() {
+const App = () => {
   const [countries, setCountries] = useState([]);
-  return <div className='App'></div>;
-}
+
+  const findCountry = (event) => {
+    event.preventDefault();
+    if (event.target.value !== '') {
+      axios
+        .get(
+          `https://restcountries.com/v3.1/name/${event.target.value}?fields=name,capital,flags,cioc,population,languages`
+        )
+        .then((response) => {
+          const cts = response.data;
+          const sorted = cts.sort((a, b) => {
+            return a.cioc - b.cioc;
+          });
+
+          setCountries(sorted);
+        })
+        .catch((error) => {
+          console.error('error: ', error.message);
+          setCountries([]);
+        });
+    } else {
+      setCountries([]);
+    }
+  };
+
+  return (
+    <div className='App'>
+      <form>
+        find countries <input type='text' onChange={findCountry} />
+      </form>
+
+      {countries.length > 9 ? (
+        'Too many matches, specify another filter'
+      ) : countries.length === 1 ? (
+        <Country country={countries[0]} />
+      ) : (
+        countries.map((country) => {
+          return <div key={country.cioc}>{country.name.common}</div>;
+        })
+      )}
+    </div>
+  );
+};
 
 export default App;
